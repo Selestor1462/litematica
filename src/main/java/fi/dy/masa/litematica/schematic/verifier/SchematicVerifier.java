0b21b7a4b3f0e645ad62a1a8b7c0974bec8479cb
Selestor1462 <danyusha.dor@gmail.com>
2026-07-21 22:18:29 +0300

    Update SchematicVerifier.java
    x2
    
    Signed-off-by: Selestor1462 <danyusha.dor@gmail.com>

--- src/main/java/fi/dy/masa/litematica/schematic/verifier/SchematicVerifier.java
+++ src/main/java/fi/dy/masa/litematica/schematic/verifier/SchematicVerifier.java
@@ -308,7 +308,7 @@ public class SchematicVerifier extends TaskBase implements IInfoHudRenderer
         this.ignoreBlockRegistry = new IgnoreBlockRegistry();
 
         this.setCompletionListener(completionListener);
-        this.requiredChunks.addAll(schematicPlacement.getTouchedChunks(SubRegionPlacement.RequiredEnabled.ENABLED));
+        this.requiredChunks.addAll(schematicPlacement.getTouchedChunks(SubRegionPlacement.RequiredEnabled.TRUE));
         this.totalRequiredChunks = this.requiredChunks.size();
         this.verificationStarted = true;
 
@@ -495,7 +495,7 @@ public class SchematicVerifier extends TaskBase implements IInfoHudRenderer
                 {
                     ChunkAccess chunkClient = this.worldClient.getChunk(pos.x, pos.z);
                     ChunkAccess chunkSchematic = this.worldSchematic.getChunk(pos.x, pos.z);
-                    Map<String, IntBoundingBox> boxes = this.schematicPlacement.getBoxesWithinChunk(pos.x, pos.z, SubRegionPlacement.RequiredEnabled.ENABLED);
+                    Map<String, IntBoundingBox> boxes = this.schematicPlacement.getBoxesWithinChunk(pos.x, pos.z, SubRegionPlacement.RequiredEnabled.TRUE);
 
                     for (IntBoundingBox box : boxes.values())
                     {
